240732c20a90a5b6521dd6abf0bc191fc3942228
Parvej Ali <Rihanali01010@gmail.com>
2022-05-25 21:29:20 +0530

    Improve ui and add login alert

--- Login.js
+++ Login.js
@@ -6,3 +6,15 @@ function myFunction() {
     x.type = "password";
   }
 }
+
+function verify() {
+  var x = document.getElementById("floatingInput").value;
+  var y = document.getElementById("myPass").value;
+  if (x == "RihanAli@gmail.com" && y == "password") {
+    console.log("Verified...");
+    alert("Veried...");
+  } else {
+    console.log("Try again...");
+  }
+  console.log("Username : "+ x +"\nPassword : "+ y)
+}
